af802497ea95ce9a825af4d14de83dd30f714161
Lakshmicharan <30784830+Lakshmicharan@users.noreply.github.com>
2017-08-10 11:37:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
+var first = {
     title: 'first',
     heading: 'first file',
     date :'aug 10 2017',
@@ -25,7 +25,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/first', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'first.html'));
+    res.send(createtemplate(first));
     
 });
 app.get('/second', function(req,res){
@@ -50,6 +50,32 @@ app.get('/ui/madi.png', function (req, res) {
 // Use 8080 only for local development if you already have apache running on 80
 
 var port = 80;
+function createtemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading =data.heading;
+    var content=data.content;
+
+var htmltemplate=`<html>
+<head>
+   <title>${title}</title>
+   <meta name="viewport" content="width:device-width,  initial-scale:1">
+   <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+ <body>
+     <div class="container">
+     <div><a href="/">Home</a></div>
+     <hr/>
+     <div> ${date}</div>
+     <div>
+        ${content}
+        
+     </div>
+     </div>
+     </body>
+ </html>`;
+ return htmltemplate;
+}
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
