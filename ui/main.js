448e331e64198c64e758ddb73d8daf4a51da29cd
Lakshmicharan <30784830+Lakshmicharan@users.noreply.github.com>
2017-08-11 00:24:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,15 @@ alert('hi');
 var element = document.getElementById('fdiv');
 element.innerHTML = 'SreeCharan';
 var img = document.getElementById('madi');
+ var marginLeft=0;
+function moveRight()
+{
+   marginLeft=marginLeft+10;
+   img.style.marginLeft=marginLeft+'px';
+}
 img.onclick = function()
 {
-    img.style.marginLeft = '100px';
+    var interval=setInterval(moveRight,100);
+    
     
 }
