b9f10b0cfbb3ed0d282e6cf8173d371a36ffe884
Lakshmicharan <30784830+Lakshmicharan@users.noreply.github.com>
2017-08-11 01:29:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ button.onclick = function()
         }
         
     };
-    request.open('GET','http:/prasannasreecharan.imad.hasura-app.io/counter',true);
+    request.open('GET','http://prasannasreecharan.imad.hasura-app.io/counter',true);
     request.send(null);
     
 }
