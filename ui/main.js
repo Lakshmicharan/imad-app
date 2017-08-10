b0db29982aa78bd13d4656b0c3abf9b5aa27ca31
Lakshmicharan <30784830+Lakshmicharan@users.noreply.github.com>
2017-08-10 19:39:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
-var element=document.getElementById('fdiv');
-element.innerHTML='SreeCharan';
+var element = document.getElementById('fdiv');
+element.innerHTML = 'SreeCharan';
+var img = document.getElementById('img');
+img.onclick = function()
+{
+    img.style.marginLeft = '100px';
+    
+}
