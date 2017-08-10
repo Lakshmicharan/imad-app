32791c6ad2641c7d8d3cf035adaed75a1f30d885
Lakshmicharan <30784830+Lakshmicharan@users.noreply.github.com>
2017-08-10 11:03:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+    title: 'first',
+    heading: 'first file',
+    date :'aug 10 2017',
+    content: 
+          `<div> aug 10 2017</div>
+     <div>
+        <p> This is my first web page  This is my first web page  This is my first web page </p>
+        <p> This is my first web page  This is my first web page  This is my first web page </p> 
+       <h1> This is my first web page </h1>
+        
+     </div>`
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
