a27ee261bd15ee7ff85801a43227d14106d8b3f4
Lakshmicharan <30784830+Lakshmicharan@users.noreply.github.com>
2017-08-30 12:08:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,8 +80,29 @@ var counter=0;
     res.send(counter.toString())});
 
 
-app.get('/:articleName' , function(req,res)  {
-    var articleName=req.params.articleName;
+app.get('/articles/:articleName' , function(req,res)  {
+   
+    pool.query("SELECT * FROM article where title="+ req.params.articleName, function(err,result)
+    {
+        if(err)
+        {
+            res.status(500).send(err.toString());
+            
+        }else
+        {
+            
+            if(result.rows.length ===0 )
+            {
+                res.status(404).send('Article Not Found');
+                
+            }else 
+            {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }   
+            
+        }
+    });
     res.send(createtemplate(series[articleName]));
     
 });
